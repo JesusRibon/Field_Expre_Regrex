720ee7099799f442dfaaa53136ba581bd4e12a6e
JESUS <jesus.ribon@zabud.com.co>
2024-09-06 09:34:26 -0500

    Se modifca la expresion regular

--- src/app/pages/field-regrex/field-regrex.component.ts
+++ src/app/pages/field-regrex/field-regrex.component.ts
@@ -1,4 +1,4 @@
-import { Component,  } from '@angular/core';
+import { Component } from '@angular/core';
 import { FormControl, FormGroup, Validators } from '@angular/forms';
 
 @Component({
@@ -14,7 +14,9 @@ export class FieldRegrexComponent {
   type: string = '';
   variable: string = '';
   message: string = '';
-  regrex = /^declare\s+([a-zA-Z][\w](?:\s*,\s*[a-zA-Z][\w]{0,14})*)\s+(entero|real|cadena|logico|fecha)\s*;$/
+  regrex =
+    /^declare\s+([a-zA-Z][a-zA-Z0-9_]{0,14}(?:\s*,\s*[a-zA-Z][a-zA-Z0-9_]{0,14})*)\s+(entero|real|cadena|logico|fecha)\s*;$/i
+
 
   constructor() {
     this.form = new FormGroup({
@@ -23,50 +25,46 @@ export class FieldRegrexComponent {
 
     this.formType = new FormGroup({
       type: new FormControl('', Validators.required),
-    })
+    });
   }
 
   validateRegrex() {
     this.error = '';
     this.success = '';
     const getField = this.form.get('fieldText')?.value;
-    if(!getField.startsWith('declare ')){
+    if (!getField.startsWith('declare ')) {
       this.error = 'La declaración debe comenzar con "declare"';
-    }
-    else if(!getField.endsWith(';')){
+    } else if (!getField.endsWith(';')) {
       this.error = 'La declaración debe finalizar con ";"';
-    }
-    else  if (!this.regrex.test(getField)) {
+    } else if (!this.regrex.test(getField)) {
       this.error = 'Estructura invalidad';
-    } 
-    else {
+    } else {
       this.success = 'Estructura validad';
     }
   }
 
-  validateTypeAndvariable(){
+  validateTypeAndvariable() {
     this.message = '';
     this.type = '';
     this.variable = '';
     const regrexType = /^(entero|real|cadena|logico|fecha)$/;
     const getType = this.formType.get('type')?.value;
-    if(regrexType.test(getType)){
+    if (regrexType.test(getType)) {
       const getField = this.form.get('fieldText')?.value;
       const typeEntered = getField.match(this.regrex);
-      if(typeEntered){
-        if(getType === typeEntered[2]){
+      if (typeEntered) {
+        if (getType === typeEntered[2]) {
           this.type = typeEntered[2];
           this.variable = typeEntered[1];
+        } else {
+          this.message =
+            'el tipo de dato ' + getType + ' no fue ingresado por el usuario';
         }
-        else{
-          this.message = 'el tipo de dato '+getType+' no fue ingresado por el usuario';
-        }
+      } else if (typeEntered === null) {
+        this.message =
+          'Si la estructura ingresada es incorrecta no se mostrara el tipo de dato y variables ingresadas';
       }
-      else if(typeEntered === null){
-        this.message = 'Si la estructura ingresada es incorrecta no se mostrara el tipo de dato y variables ingresadas';
-      }
-    }
-    else{
+    } else {
       this.message = 'El tipo de dato es incorrecto';
     }
   }
